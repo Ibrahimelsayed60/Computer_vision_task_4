8152b79500a47b25f02f6744c8040a1aec207739
mariembador <43917000+mariembador@users.noreply.github.com>
2021-05-17 13:46:23 +0200

    k-means

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pyqtgraph as pg
 import numpy as np
 import threshold
 import cv2
-
+import segmentation
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(ApplicationWindow, self).__init__()
@@ -21,14 +21,22 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.ui.widget_1.getPlotItem().hideAxis('left')
         self.ui.widget_3.getPlotItem().hideAxis('bottom')
         self.ui.widget_3.getPlotItem().hideAxis('left')
+        self.ui.widget.getPlotItem().hideAxis('bottom')
+        self.ui.widget.getPlotItem().hideAxis('left')
+        self.ui.widget_4.getPlotItem().hideAxis('bottom')
+        self.ui.widget_4.getPlotItem().hideAxis('left')
 
         self.image_1 = cv2.rotate(cv2.imread("Threshold images\Lenna.png",0),cv2.ROTATE_90_CLOCKWISE)
         self.image_2 = cv2.rotate(cv2.imread("Threshold images\MRIbrain1.jpg",0),cv2.ROTATE_90_CLOCKWISE)
+        image_3 = cv2.rotate(cv2.imread("Segmentation Images/sea.jpeg"),cv2.ROTATE_90_CLOCKWISE)
+        self.image_3 = cv2.cvtColor(image_3, cv2.COLOR_RGB2Luv)
+        out = pg.ImageItem(self.image_3)
+        self.ui.widget.addItem(out)
 
         self.ui.pushButton_1.clicked.connect(self.doing_otsu_global)
         self.ui.pushButton_2.clicked.connect(self.doing_otsu_local)
         self.ui.pushButton_3.clicked.connect(self.doing_original_image)
-        
+        self.ui.comboBox.currentIndexChanged[int].connect(self.segmentaion)
 
 
     def doing_global_threshold(self):
@@ -59,6 +67,22 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         out = pg.ImageItem(new_img)
         self.ui.widget_1.addItem(out)
         
+    def segmentaion(self):
+        if self.ui.comboBox.currentIndex() == 0:
+            self.ui.widget_4.clear()
+        if self.ui.comboBox.currentIndex() == 1:
+            pixel_values = self.image_3.reshape((-1, 3))
+            pixel_values = np.float32(pixel_values)
+            k = segmentation.KMeans(K=3, max_iters=100) 
+            y_pred = k.predict(pixel_values)
+            centers = np.uint8(k.cent())
+            y_pred = y_pred.astype(int)
+            labels = y_pred.flatten()
+            segmented_image = centers[labels.flatten()]
+            segmented_image = segmented_image.reshape(self.image_3.shape)
+            out = pg.ImageItem(segmented_image)
+            self.ui.widget_4.addItem(out)
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
